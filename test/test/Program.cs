28d76449dc4fd892963b5a9a0efc91deedc20f15
Ildar Faizullin <ifaizullin@acumatica.com>
2017-03-20 15:34:54 +0300

    nunit

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
 using System.Collections.Generic;
@@ -10,12 +11,35 @@ namespace test
 {
     class Program
     {
+        IWebDriver driver = new ChromeDriver();
         static void Main(string[] args)
         {
-            IWebDriver driver = new ChromeDriver();
+            
+        }
+        [SetUp]
+        public void Initialize()
+        {
             driver.Navigate().GoToUrl("https://google.com");
+            Console.WriteLine("go to url");
+        }
+        [Test]
+        public void ExecuteTest()
+        {
             IWebElement element = driver.FindElement(By.Name("q"));
             element.SendKeys("test");
+            Console.WriteLine("input");
         }
+        [Test]
+        public void NextTest()
+        {
+            Console.WriteLine("next");
+        }
+        [TearDown]
+        public void Cleanup()
+        {
+            driver.Close();
+            Console.WriteLine("close");
+        }
+
     }
 }
